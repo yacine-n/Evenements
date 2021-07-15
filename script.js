8e1fc22167556f17b7b95f947ede8e7fd8c08389
Yacine Ndiaye <65970214+yacine-n@users.noreply.github.com>
2021-07-15 06:11:01 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+/*********ciblant l'élément*************/
+var image= document.getElementsByTagName("img"); //retourne un tableau d'objet
+var img = image[0];
+
+img.addEventListener("click", function init() {
+  alert("nous vous aidons à rester chez vous pour sauver des vies grace à nos programmes");
+});
+
+
+
+/*var image= document.getElementsByTagName("img"); //retourne un tableau d'objet
+var img = image[0];
+
+img.addEventListener("click", init);
+function init() {
+  alert("nous vous aidons à rester chez vous pour sauver des vies grace à nos programmes");
+}*/
